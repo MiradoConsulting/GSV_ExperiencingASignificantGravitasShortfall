f22ba0f59389df7f673e8f3e8bd9f4f342871f0b
anders martini <andersmartini2@gmail.com>
2020-03-06 22:40:29 +0100

    dodgebot

--- src/main/java/SleeperService.java
+++ src/main/java/SleeperService.java
@@ -1,81 +1,38 @@
 import robocode.*;
-//import java.awt.Color;
 
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+public class SleeperService extends AdvancedRobot {
+        double previousEnergy = 100;
+        int movementDirection = 1;
+        int gunDirection = 1;
+        public void run() {
+            setTurnGunRight(99999);
+        }
+        public void onScannedRobot(
+                ScannedRobotEvent e) {
+            // Stay at right angles to the opponent
+            setTurnRight(e.getBearing()+90-
+                    30*movementDirection);
 
-/**
- * GSV - a robot by (your name here)
- */
-public class SleeperService extends AdvancedRobot
-{
+            // If the bot has small energy drop,
+            // assume it fired
+            double changeInEnergy =
+                    previousEnergy-e.getEnergy();
+            if (changeInEnergy>0 &&
+                    changeInEnergy<=3) {
+                // Dodge!
+                movementDirection =
+                        -movementDirection;
+                setAhead((e.getDistance()/4+25)*movementDirection);
+            }
+            // When a bot is spotted,
+            // sweep the gun and radar
+            gunDirection = -gunDirection;
+            setTurnGunRight(99999*gunDirection);
 
-	double lastSeenRobotBearnig = 0;
+            // Fire directly at target
+            fire ( 2 ) ;
 
-	
-	/**
-	 * run: GSV's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			turnRadarRight(360);
-			
-	
-			
-
-		}
-	}
-	
-
-
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-public void onScannedRobot(ScannedRobotEvent e) {	
-		setTurnRadarRight(getHeading() - e.getBearing() + getRadarHeading() );
-		setTurnRight(e.getBearing());
-		if (e.getDistance() > 200) {
-			setAhead(200);
-		} else if (e.getDistance() > 150) {
-			setAhead(100);
-		} else{ 
-			fire(10);
-		}	
-	}
-	
-
-	private void circleAndKill(ScannedRobotEvent e){
-		setTurnRight(e.getBearing() + 90 );
-		setAhead(150);
-		double lturn = (getHeading() - e.getBearing() ) + getGunHeading() ;
-		if(lturn < 180){
-			turnGunLeft(lturn);
-		}else{
-			turnGunRight(360 - lturn);
-		}
-
-		fire(1);
-	}
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-
-	}	
+            // Track the energy level
+            previousEnergy = e.getEnergy();
+        }
 }
